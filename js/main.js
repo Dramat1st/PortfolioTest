5977817073a5092f907ce2a12779990858168713
Dramat1st <39029593+Dramat1st@users.noreply.github.com>
2021-12-07 17:15:52 +0300

    Change background video

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // // $(window).on('load', function()
 window.onload = function(){
     //vide.js - video background
-    $('#header').vide('./video/cover4', {
+    $('#header').vide('./video/cover1', {
         bgColor: '#8d89a4'
     });
 
