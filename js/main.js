d342e71f1ed05a6413a705e53d4e8abe0dd2bf6c
Dramat1st <39029593+Dramat1st@users.noreply.github.com>
2021-12-07 22:27:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // window.onload = function()
-$(window).on('load', function()
-{
-    //vide.js - video background
+$(window).on('load', function(){
+   
     $('#header').vide('./video/cover1', {
         bgColor: '#8d89a4'
     });
 
-};
+});
