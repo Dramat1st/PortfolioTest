c49b2efd29ecc164d83ef7e1a226d854683e8085
Dramat1st <39029593+Dramat1st@users.noreply.github.com>
2021-12-07 17:09:23 +0300

    New site test

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// // $(window).on('load', function()
+window.onload = function(){
+    //vide.js - video background
+    $('#header').vide('./video/cover4', {
+        bgColor: '#8d89a4'
+    });
+
+};
+
+
+// window.onload = function(){
+//     //vide.js - video background
+//     const elem = document.getElementById('header');
+//     elem.vide('./video/cover4', {
+//         bgColor: '#8d89a4'
+//     });
+
+// };
+
+
+
+// window.onload = function(){
+//     document.getElementById('header').vide('./video/cover4', {
+//         bgColor: '#8d89a4'
+//     });
+// }
